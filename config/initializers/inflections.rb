1cdb08017b9a21d5eb3eae8767407ceab43fd2d7
John Armitage <john.a@familya.co.uk>
2014-04-30 17:32:11 +0100

    beta s8

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,17 +6,17 @@
 ActiveSupport::Inflector.inflections(:en) do |inflect|
    #inflect.plural /^(ox)$/i, '\1en'
    #inflect.singular /^(ox)en/i, '\1'
-  inflect.irregular 'Caribbean', 'Caribbean'
-  inflect.irregular 'Urban Country', 'Urban Country'
-  inflect.irregular 'MiPac', 'MiPac'
-  inflect.irregular 'Sweet Trolley', 'Sweet Trolley'
-  inflect.irregular 'headwear', 'headwear'
-  inflect.irregular 'Food', 'Food'
-  inflect.irregular 'Scented', 'Scented'
-  #inflect.irregular 'Art', 'Art'      # messes up cart schema!!
-  inflect.irregular 'Fibre', 'Fibre'
-  inflect.irregular 'catalogue', 'catalogue'
-  inflect.uncountable %w(jewellery sundry art stuff)
+  # inflect.irregular 'Caribbean', 'Caribbean'
+  # inflect.irregular 'Urban Country', 'Urban Country'
+  # inflect.irregular 'MiPac', 'MiPac'
+  # inflect.irregular 'Sweet Trolley', 'Sweet Trolley'
+  # inflect.irregular 'headwear', 'headwear'
+  # inflect.irregular 'Food', 'Food'
+  # inflect.irregular 'Scented', 'Scented'
+  # #inflect.irregular 'Art', 'Art'      # messes up cart schema!!
+  # inflect.irregular 'Fibre', 'Fibre'
+  # inflect.irregular 'catalogue', 'catalogue'
+  inflect.uncountable %w(jewellery sundry art stuff collection catalogue fibre scented food headwear trolley MiPac 'Urban Country' caribbean)
 end
 
 # These inflection rules are supported but not enabled by default:
