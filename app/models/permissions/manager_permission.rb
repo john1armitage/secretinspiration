e54a07e9d406dab7d54fb3c4699d559440082d61
John Armitage <john.a@familya.co.uk>
2015-09-27 13:55:01 +0100

    order permissions

--- app/models/permissions/manager_permission.rb
+++ app/models/permissions/manager_permission.rb
@@ -21,7 +21,7 @@ module Permissions
       allow_param :booking, [:walkin, :arrival, :pax, :customer_name, :contact, :notes, :booking_date, :state, :email, :window, :confirmed]
       allow_param :meal, [:contact, :phone, :state, :start_time, :notes]
       allow_param :message, [:message, :message_type, :user_id, :create_time]
-      allow_param :order, [:state, :paid, :credit_card, :tip]
+      allow_param :order, [:state, :net_total, :net_home, :tax_home, :paid, :credit_card, :tip, :voucher, :goods, :cheque, :cash, :net_total_cents, :net_home_cents, :tax_home_cents, :paid_cents, :credit_card_cents, :tip_cents, :voucher_cents, :goods_cents, :cheque_cents, :cash_cents ]
       allow_param :daily, [:headcount, :account_date, :credit_card, :session, :pax, :till, :surplus, :walkin_pax, :updated_at]
       allow_param :timesheet, [:employee_id, :start_time, :end_time, :hours, :work_date, :session, :rate_cents, :headcount]
       #allow :choices, :index
